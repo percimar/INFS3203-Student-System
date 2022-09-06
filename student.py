bfea6e942bb90c4056a982f49de14f879ec783de
Asmar <Asmar@email.com>
2022-09-06 14:48:06 +0300

    Finished Student class

---
+++ student.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, age, id):
+        if type(name) != str:
+            raise TypeError("Name should be a string.")
+        if type(age) != int:
+            raise TypeError("Age should be an int.")
+        if type(id) != int:
+            raise TypeError("ID should be an int.")
+        self._name = name
+        self._age = age
+        self._id = id
+    
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
