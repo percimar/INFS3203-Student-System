0b1e2ca4d022de1b0d7198c0b89b8219a473e742
Asmar <Asmar@email.com>
2022-09-06 15:24:10 +0300

    added explanations for tests

--- test_student.py
+++ test_student.py
@@ -3,43 +3,52 @@ from student import Student
 
 class TestStudent(TestCase):
     def test_id_getter(self):
+        """Checks that id getter works without error"""
         test_student = Student("Asmar", 23, 1)
 
         self.assertEqual(test_student.id, 1)
 
     
     def test_name_getter(self):
+        """Checks that name getter works without error"""
         test_student = Student("Asmar", 23, 1)
 
         self.assertEqual(test_student.name, "Asmar")
 
     
     def test_age_getter(self):
+        """Checks that age getter works without error"""
         test_student = Student("Asmar", 23, 1)
 
         self.assertEqual(test_student.age, 23)
     
     def test_negative_id_exception(self):
+        """Checks that negative id results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", 23, -1)
     
     def test_negative_age_exception(self):
+        """Checks that negative age results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", -1, 1)
 
     def test_no_id_exception(self):
+        """Checks that creating a student without an id results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", 23)
     
     def test_no_name_exception(self):
+        """Checks that creating a student without a name results in a TypeError"""
         with self.assertRaises(TypeError):
             Student(age=23, id=1)
     
     def test_no_age_exception(self):
+        """Checks that creating a student without an age results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", id=1)
     
     def test_wrong_id_type_exception(self):
+        """Checks that not having id as an integer results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", 23, "1")
         
@@ -50,6 +59,7 @@ class TestStudent(TestCase):
             Student("Asmar", 23, 1.0)
     
     def test_wrong_age_type_exception(self):
+        """Checks that not having age as an integer results in a TypeError"""
         with self.assertRaises(TypeError):
             Student("Asmar", "23", 1)
         
@@ -60,6 +70,7 @@ class TestStudent(TestCase):
             Student("Asmar", 23.0, 1)
         
     def test_wrong_name_type_exception(self):
+        """Checks that not having name as a string results in a TypeError"""
         with self.assertRaises(TypeError):
             Student(True, 23, 1)
         
--- test_student_system.py
+++ test_student_system.py
@@ -3,6 +3,10 @@ from student_system import StudentSystem, Student
 
 class TestStudentSystem(TestCase):
     def test_add_student_success(self):
+        """
+        Checks that adding a student works without error
+        Accesses system internals to ensure student was added.
+        """
         student_system = StudentSystem()
         test_student = Student("Asmar", 23, 1)
 
@@ -12,6 +16,9 @@ class TestStudentSystem(TestCase):
 
     
     def test_find_student_success(self):
+        """
+        Checks that finding a student produces works without error
+        """
         student_system = StudentSystem()
         test_student = Student("Asmar", 23, 1)
 
@@ -20,6 +27,11 @@ class TestStudentSystem(TestCase):
         self.assertIs(student_system.find_student(1), test_student)
     
     def test_delete_student_success(self):
+        """
+        Checks that deleting a student produces works without error
+        delete_student() should return True if deletion was successful
+        Accesses system internals to ensure student was deleted.
+        """
         student_system = StudentSystem()
         test_student = Student("Asmar", 23, 1)
 
@@ -29,6 +41,9 @@ class TestStudentSystem(TestCase):
         self.assertFalse(test_student.id in student_system.students)
 
     def test_add_student_exception(self):
+        """
+        Checks that adding a student incorrectly results in a TypeError
+        """
         student_system = StudentSystem()
         
         with self.assertRaises(TypeError):
@@ -36,6 +51,9 @@ class TestStudentSystem(TestCase):
 
     
     def test_find_student_not_exists(self):
+        """
+        Checks that finding a student with a non-existent id returns None
+        """
         student_system = StudentSystem()
         
         self.assertEquals(student_system.find_student(1), None)
@@ -47,6 +65,9 @@ class TestStudentSystem(TestCase):
 
     
     def test_delete_student_not_exists(self):
+        """
+        Checks that deleting a student with a non-existent id returns False
+        """
         student_system = StudentSystem()
         
         self.assertFalse(student_system.delete_student(1))
